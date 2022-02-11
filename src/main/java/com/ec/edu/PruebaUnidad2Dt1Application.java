ed4cbaf5022dd71a630eb48aa97008de39987b7f
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2022-02-11 18:25:24 -0500

    prueba2

---
+++ src/main/java/com/ec/edu/PruebaUnidad2Dt1Application.java
@@ -0,0 +1,15 @@
+package com.ec.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaUnidad2Dt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaUnidad2Dt1Application.class, args);
+	}
+	
+	//prueba
+
+}
---
+++ src/test/java/com/ec/edu/PruebaUnidad2Dt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaUnidad2Dt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
